2f546abb72d51128d376b4737ae375845bcf8abf
tl <tl@hust.com>
2016-04-01 09:44:01 +0800

    change navigation drawer label text

--- app/src/main/java/com/example/a/tower/MainActivity.java
+++ app/src/main/java/com/example/a/tower/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_phone_info) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_current_network) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_base_map) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_alert_area) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_about) {
 
         }
 
