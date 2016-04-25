fb73aa35e9243ed4f597aa4265e4929685ffe53a
tl <tl@hust.com>
2016-04-25 09:35:12 +0800

    save logcat in sdcard files to help debug

--- app/src/main/java/com/example/a/tower/Tower.java
+++ app/src/main/java/com/example/a/tower/Tower.java
@@ -1,8 +1,12 @@
 package com.example.a.tower;
 
 import android.app.Application;
+import android.os.Environment;
 
 import com.baidu.mapapi.SDKInitializer;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by a on 2016/3/31.
@@ -11,7 +15,63 @@ public class Tower extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        saveLogcatToFile();
+
         // 在使用 SDK 各组间之前初始化 context 信息，传入 ApplicationContext
         SDKInitializer.initialize(this);
+    }
+
+    private void saveLogcatToFile() {
+
+        if ( isExternalStorageWritable() ) {
+
+            File appDirectory = new File( Environment.getExternalStorageDirectory() + "/Tower" );
+            File logDirectory = new File( appDirectory + "/log" );
+            File logFile = new File( logDirectory, "logcat" + System.currentTimeMillis() + ".txt" );
+
+            // create app folder
+            if ( !appDirectory.exists() ) {
+                appDirectory.mkdir();
+            }
+
+            // create log folder
+            if ( !logDirectory.exists() ) {
+                logDirectory.mkdir();
+            }
+
+            // clear the previous logcat and then write the new one to the file
+            try {
+                Process process = Runtime.getRuntime().exec( "logcat -c");
+                // note *:D just save all debug log to log file. see logcat --help for how to filer log
+                //process = Runtime.getRuntime().exec( "logcat -f " + logFile + " *:S com.example.a.tower.MainActivity:D");
+                process = Runtime.getRuntime().exec( "logcat -f " + logFile + " *:D");
+            } catch ( IOException e ) {
+                e.printStackTrace();
+            }
+
+        } else if ( isExternalStorageReadable() ) {
+            // only readable
+        } else {
+            // not accessible
+        }
+    }
+
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if ( Environment.MEDIA_MOUNTED.equals( state ) ) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if ( Environment.MEDIA_MOUNTED.equals( state ) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals( state ) ) {
+            return true;
+        }
+        return false;
     }
 }
